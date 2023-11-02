bea9fb959f2caabed8b90ec9d7ba8131543a61ac
jorge <jorge.ramirez.br@gmail.com>
2023-11-02 02:20:34 +0100

    succes percentage

--- diceGame/database/migrations/2014_10_12_000000_create_users_table.php
+++ diceGame/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nickname');
             $table->string('email')->unique();
+            $table->float('succes_percentage')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
