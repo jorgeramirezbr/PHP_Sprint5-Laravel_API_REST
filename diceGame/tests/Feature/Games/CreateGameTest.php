daca8fb343b8db293f7af0a703198092bc93f952
jorge <jorge.ramirez.br@gmail.com>
2023-11-02 10:06:40 +0100

    tdd CreateGameTest / assertions adding

--- diceGame/tests/Feature/Games/CreateGameTest.php
+++ diceGame/tests/Feature/Games/CreateGameTest.php
@@ -16,15 +16,29 @@ class CreateGameTest extends TestCase
     public function a_game_can_be_created(): void
     {
         $this->withoutExceptionHandling(); 
+
         $user = User::factory()->create();
         $response = $this->post('/api/players/'.$user->id.'/games/');
-
         $response->assertStatus(200);
         $this->assertCount(1, Game::all());
         $game = Game::first();
-
         $this->assertEquals($game->player_id, $user->id);
-        //$this->assertEquals($user->email, 'coco@mail.com');
-        //$this->assertEquals($user->password, '123456789');
+
+        // probamos con un segundo usuario
+        $user2 = User::factory()->create();
+        $response2 = $this->post('/api/players/'.$user2->id.'/games/');
+        $response2->assertStatus(200);
+        $this->assertCount(2, Game::all());
+        $game2 = Game::find(2);
+        $this->assertEquals($game2->player_id, $user2->id);
+
+        // probamos con un segundo game en un mismo usuario
+        $response3 = $this->post('/api/players/'.$user->id.'/games/');
+        $response3->assertStatus(200);
+        $this->assertCount(3, Game::all());
+        $game3 = Game::find(3);
+        $this->assertEquals($game3->player_id, $user->id);
+
+        $this->assertCount(2, User::all());   //mantenemos aun 2 usuarios
     }
 }
